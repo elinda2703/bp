8de42eb241a3ee0b2747d854c9343acff40b8bc8
elinda <ela.pospechova@gmail.com>
2024-03-27 00:14:30 +0100

    menší improvement

--- scripts/playground.py
+++ scripts/playground.py
@@ -1,79 +1,159 @@
 import fiona
+import time
 import shapely
 from math import sqrt,inf
 import numpy as np
 from shapely.geometry import shape, mapping, LineString
+from matplotlib import pyplot as plt 
 
-def pythagoras(x1, y1, x2, y2):
-    # Computes distance between two points using Pythagoras theorem.
-    distance = sqrt((x1-x2)**2+(y1-y2)**2)
-    return distance
+start=time.time()
 
-with fiona.open("data/vzorecek_leva.shp") as levy_breh:
-    for feature in levy_breh:
-        geometry = shape(feature['geometry'])
-        leva_array = np.array(geometry.coords)
+with fiona.open("data/meandry_leva.shp") as levy_breh:
+    linestring_left=shape(levy_breh[0]['geometry'])
+        
+        
+with fiona.open("data/meandry_prava.shp") as pravy_breh:
+    linestring_right=shape(pravy_breh[0]['geometry'])
+    
+    
+seg_linestring_left=shapely.segmentize(linestring_left,25)
+seg_linestring_right=shapely.segmentize(linestring_right,25)
 
-with fiona.open("data/vzorecek_prava.shp") as pravy_breh:
-    for feature in pravy_breh:
-        geometry = shape(feature['geometry'])
-        prava_array=np.array(geometry.coords)
 
-result_lp = []
-result_pl=[]
+array_left=np.array(seg_linestring_left.coords)
+print(len(array_left))
+array_right=np.array(seg_linestring_right.coords)
+array_right=np.flipud(array_right)
+print(len(array_right))
 
-n=0
-for point1 in leva_array:
-    shortest_dist=inf
-    m=0
-    for point2 in prava_array:
-        distance = pythagoras(point1[0],point1[1],point2[0],point2[1])
-        if distance<shortest_dist:
-            shortest_dist=distance
-            radek={"vertex":{"index":n,"souradnice":point1,"closest":{"1nn":{"index":m,"souradnice":point2,"vzdalenost":distance}}}}
-        m+=1
-    result_lp.append(radek)
-    n+=1
 
-n=0
-for point1 in prava_array:
-    shortest_dist=inf
-    m=0
-    for point2 in leva_array:
-        distance = pythagoras(point1[0],point1[1],point2[0],point2[1])
-        if distance<shortest_dist:
-            shortest_dist=distance
-            radek={"vertex":{"index":n,"souradnice":point1,"closest":{"1nn":{"index":m,"souradnice":point2,"vzdalenost":distance}}}}
-        m+=1
-    result_pl.append(radek)
-    n+=1
-#print(result_pl[231])
-pricky=[]
-for row_lp in result_lp:
-    index_q_lp=row_lp["vertex"]["index"]
-    index_p_lp=row_lp["vertex"]["closest"]["1nn"]["index"]
-    for row_pl in result_pl:
-        index_q_pl=row_pl["vertex"]["index"]
-        index_p_pl=row_pl["vertex"]["closest"]["1nn"]["index"]
-        if index_q_lp==index_p_pl and index_p_lp==index_q_pl:
-            coords_leva=row_lp["vertex"]["souradnice"]
-            coords_prava=row_pl["vertex"]["souradnice"]
-            pricka=LineString([coords_leva,coords_prava])
-            pricky.append(pricka)
-print (len(pricky))
+class NearestNeighborsCouple (object):
+    def __init__(self, p_point, query_point, distance):
+        self.p=p_point
+        self.q=query_point
+        self.distance=distance
+    
+    @classmethod
+    def get_nn_couple (cls, p_points, q_point):
+        current_min_dist = inf
+        for p in p_points:
+            dist = np.linalg.norm(p - q_point)
+            if dist<current_min_dist:
+                current_min_dist=dist
+                nn_couple=NearestNeighborsCouple(p,q_point,dist)
+        return nn_couple
 
+def get_nn_list(p_points, q_points):
+    nn=[]
+    for point in q_points:
+        nnc=NearestNeighborsCouple.get_nn_couple(p_points,point)
+        nn.append(nnc)
+    return nn
+
+
+def get_all_pricky(array_left,array_right,left_start,right_start,left_end,right_end,pricky=None):
+    if pricky is None:
+        pricky = {}
+    #nn_lr=get_nn_list(array_right[right_start:right_end],array_left[left_start:left_end])
+    #nn_rl=get_nn_list(array_left[left_start:left_end],array_right[right_start:right_end])
+
+    left_splits=[]
+    right_splits=[]
+    #print (len(nn_lr))
+    #print (len(nn_rl))
+    
+    for vertex0 in array_left[left_start:left_end]:
+        nnc=NearestNeighborsCouple.get_nn_couple(array_right[right_start:right_end],vertex0)
+        for vertex1 in array_left[left_start:left_end]:
+            if all(vertex1!=vertex0):
+                dist=np.linalg.norm(vertex1 - nnc.p)
+                if dist<nnc.distance:
+                    break
+                else:
+                    continue
+        if dist>nnc.distance:                
+            left_index = np.where(np.all(array_left == vertex0, axis=1))[0][0]
+            right_index = np.where(np.all(array_right == nnc.p, axis=1))[0][0]
+            pricka=[vertex0,nnc.p]                    
+            pricky[left_index]=pricka
+            left_splits.append(left_index)
+            right_splits.append(right_index)
+            
+        
+    #print(len(pricky))
+    #asi checknout krizeni    
+    left_splits.append(left_end)
+    right_splits.append(right_end)
+    
+    lower_index_left=None
+    lower_index_right=None
+        
+    for left_split,right_split in zip(left_splits,right_splits):
+        if lower_index_left==None and lower_index_right==None:
+            lower_index_left=left_start
+            lower_index_right=right_end
+        else:
+            lower_index_left=upper_index_left+1
+            lower_index_right=upper_index_right+1
+            
+        upper_index_left=left_split
+        upper_index_right=right_split    
+        if upper_index_left-lower_index_left >=2 and upper_index_right-lower_index_right >=2:
+            get_all_pricky(array_left,array_right,lower_index_left,lower_index_right,upper_index_left,upper_index_right,pricky)
+    return pricky
+
+
+pricky_final=get_all_pricky(array_left,array_right,0,0,len(array_left),len(array_right))        
+
+sorted_pricky_final = dict(sorted(pricky_final.items()))
+
+sorted_pricky_list=list(sorted_pricky_final.values())
+
+final_array=np.array(sorted_pricky_list)
+
+widths = np.linalg.norm(np.diff(final_array, axis=1), axis=-1)
+
+banks=final_array.swapaxes(0,1)
+
+skoro_kilometraz=np.linalg.norm(np.diff(banks, axis=1), axis=-1)
+
+distances_between_transects=np.mean(skoro_kilometraz,axis=0)/1000
+
+cum_km=np.cumsum(distances_between_transects)
+
+cum_km0=np.insert(cum_km,0,0)
+
+end=time.time()
+
+plt.title("Matplotlib demo") 
+plt.xlabel("kilometrage (km)") 
+plt.ylabel("river width (m)") 
+plt.plot(cum_km0,widths)
+plt.grid() 
+plt.show()
+
+
+total_time = end - start
+print("\n"+ str(total_time))
+#print(pricky_final)    
 schema = {
     'geometry': 'LineString',
-    'properties': {'id': 'int'},
 }
 # Write a new Shapefile
 with fiona.open('data/vysledek.shp', 'w', 'ESRI Shapefile', schema) as c:
     ## If there are multiple geometries, put the "for" loop here
-    x=0
-    for item in pricky:
-        c.write({
-            'geometry': mapping(item),
-            'properties': {'id': x},
-        })
-        x+=1
 
+    for pricka in final_array:
+        pricka_linestring=LineString(pricka)
+        c.write({
+            'geometry': mapping(pricka_linestring),
+        })
+
+with fiona.open('data/novy_brehy.shp', 'w', 'ESRI Shapefile', schema) as c:
+    ## If there are multiple geometries, put the "for" loop here
+
+    for bank in banks:
+        bank_linestring=LineString(bank)
+        c.write({
+            'geometry': mapping(bank_linestring),
+        })
